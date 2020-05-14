f23d547c63a41a8b357d9cec8c64dc7375f2f86b
Nam Bui Tran Thanh <nambuitranthanh@Nams-MacBook-Pro.local>
2020-05-15 00:18:17 +0700

    class component to functional com

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { } from 'react'
 import Logo from '../../assets/images/stories-logo.svg'
 import './Header.css'
 
@@ -6,27 +6,25 @@ import { Link } from 'react-router-dom'
 import { Menu, Image } from 'semantic-ui-react'
 import ButtonAuthentication from './ButtonAuthentication/ButtonAuthentication'
 
-export default class Header extends Component {
-   render() {
-      return (
-         <div className="header-wrapper">
-            <div className="ui container">
-               <Menu secondary className="site-header">
-                  <Menu.Item name="logo" className="logo" as={Link} to="/">
-                     <Image src={Logo} alt="Logo" />
+export default () => {
+   return (
+      <div className="header-wrapper">
+         <div className="ui container">
+            <Menu secondary className="site-header">
+               <Menu.Item name="logo" className="logo" as={Link} to="/">
+                  <Image src={Logo} alt="Logo" />
+               </Menu.Item>
+               <Menu.Item name="topStories">
+                  <Link to="/">Top Stories</Link>
+               </Menu.Item>
+               <Menu.Menu position="right" className="sign-in">
+                  <Menu.Item name="signin">
+                     <ButtonAuthentication />
                   </Menu.Item>
-                  <Menu.Item name="topStories">
-                     <Link to="/">Top Stories</Link>
-                  </Menu.Item>
-                  <Menu.Menu position="right" className="sign-in">
-                     <Menu.Item name="signin">
-                        <ButtonAuthentication />
-                     </Menu.Item>
-                  </Menu.Menu>
-               </Menu>
-            </div>
+               </Menu.Menu>
+            </Menu>
          </div>
+      </div>
 
-      )
-   }
+   )
 }
--- src/components/TopStories/TopStories.js
+++ src/components/TopStories/TopStories.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Segment, Header, Item } from 'semantic-ui-react'
 import StorySummaryCompact from '../StorySummaryCompact/StorySummaryCompact'
@@ -7,24 +7,21 @@ import './TopStories.css'
 
 import { loadPopularArticles } from '../../redux/actions/articleActions'
 
-class TopStories extends React.Component {
+const TopStories = ({ loadPopularArticles, articles }) => {
+   useEffect(() => {
+      loadPopularArticles();
+   }, [loadPopularArticles]);
 
-   componentDidMount() {
-      this.props.loadPopularArticles();
-   }
-
-   render() {
-      return (
-         <Segment>
-            <Header as="h3">Top Stories</Header>
-            <Item.Group divided className="story-list">
-               {this.props.articles.map(item =>
-                  <StorySummaryCompact key={item._id} article={item} />
-               )}
-            </Item.Group>
-         </Segment>
-      )
-   }
+   return (
+      <Segment>
+         <Header as="h3">Top Stories</Header>
+         <Item.Group divided className="story-list">
+            {articles.map(item =>
+               <StorySummaryCompact key={item._id} article={item} />
+            )}
+         </Item.Group>
+      </Segment>
+   )
 
 }
 
